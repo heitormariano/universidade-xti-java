c8de86f16ff7604a10e92a09aec247731168d084
Heitor Mariano <heitormariano.camara@gmail.com>
2025-12-18 20:45:57 -0300

    revisao pontual de classes

--- src/br/com/xti/aula027/Fluxo.java
+++ src/br/com/xti/aula027/Fluxo.java
@@ -4,36 +4,35 @@ import java.util.ArrayList;
 
 public class Fluxo {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		int[] pares = { 2, 4, 6, 8 };
+        int[] pares = {2, 4, 6, 8};
 
-		System.out.println("Numeros pares:");
+        System.out.println("Numeros pares:");
 
-		for (int i = 0; i < pares.length; i++) {
-			int par = pares[i];
-			System.out.println(par);
-		}
+        for (int i = 0; i < pares.length; i++) {
+            int par = pares[i];
+            System.out.println(par);
+        }
 
-		System.out.println("Numeros pares [uso do foreach]:");
+        System.out.println("Numeros pares [uso do foreach]:");
 
-		// recuperando cada elemento do array de numeros pares
-		for (int numPar : pares) {
-			System.out.println(numPar);
-		}
+        // recuperando cada elemento do array de numeros pares
+        for (int numPar : pares) {
+            System.out.println(numPar);
+        }
 
-		System.out.println();
+        System.out.println();
 
-		System.out.println("Elementos da lista:");
-		ArrayList<Integer> lista = new ArrayList<>();
+        System.out.println("Elementos da lista:");
+        ArrayList<Integer> lista = new ArrayList<>();
 
-		for (int i = 0; i < 10; i++) {
-			lista.add(i);
-		}
-        
-		for (int numero : lista) {
-			System.out.println(numero);
-		}
+        for (int i = 0; i < 10; i++) {
+            lista.add(i);
+        }
 
-	}
+        for (int numero : lista) {
+            System.out.println(numero);
+        }
+    }
 }
--- src/br/com/xti/aula028/Fluxo.java
+++ src/br/com/xti/aula028/Fluxo.java
@@ -7,58 +7,54 @@ import java.util.stream.Collectors;
 
 public class Fluxo {
 
-	// estudo while e do while. Sao semelhantes ao for
+    // Estudo while e do while. Sao semelhantes ao for
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		int num = 0;
+        int num = 0;
 
-		// expressao equivale: for (int i=0;i<2;i++)
-		while (num < 2) {
+        // expressao equivale: for (int i=0;i<2;i++)
+        while (num < 2) {
+            System.out.println("valor do numero:" + num);
+            num++; // para evitar um loop infinito, precisamos atualizar o valor de "num"
+        }
 
-			System.out.println("valor do numero:" + num);
-			num++; // para evitar um loop infinito, precisamos atualizar o valor de "num"
-		}
+        // nessa expressao o que consta no "do" eh executado pelo menos uma vez
+        int num2 = 3;
+        do {
+            System.out.println("Imprimindo valor: " + num2);
+        } while (num2 < 2);
 
-		// nessa expressao o que consta no "do" eh executado pelo menos uma vez
+        System.out.println();
 
-		int num2 = 3;
+        System.out.println("Programa Exemplo");
+        ArrayList<String> produtos = new ArrayList<>();
 
-		do {
-			System.out.println(num2);
-		} while (num < 2);
+        Scanner scanner = new Scanner(System.in);
 
-		System.out.println();
-		System.out.println("Programa Exemplo");
+        System.out.println("Digite os nomes dos produtos desejados. Para finalizar, digite FIM");
 
-		ArrayList<String> produtos = new ArrayList<String>();
+        String produto;
+        while (!(produto = scanner.nextLine()).equalsIgnoreCase("fim")) {
+            produtos.add(produto);
+        }
+        System.out.println("Fim da recuperacao dos nomes dos produtos");
 
-		Scanner scanner = new Scanner(System.in);
+        System.out.println("Lista de produtos obtida");
+        System.out.println(produtos);
+        System.out.println();
 
-		System.out.println("Liste seus produtos e para sair digite FIM");
+        int numLer = -1;
+        List<Integer> numeros = new ArrayList<>();
 
-		String produto;
-		while (!(produto = scanner.nextLine()).equalsIgnoreCase("fim")) {
-			produtos.add(produto);
-		}
+        System.out.println("Informe os numeros que precisa em sequencia. Para encerrar, digite zero");
+        while ((numLer = scanner.nextInt()) != 0) {
+            numeros.add(numLer);
+        }
+        System.out.println("Fim da recuperacao dos numeros");
 
-		System.out.println("Fim da recuperacao dos nomes dos produtos\n");
-
-		System.out.println("Lista de nomes obtida");
-		System.out.println(produtos.toString());
-
-		int numLer = -1;
-		List<Integer> numeros = new ArrayList<Integer>();
-
-		System.out.println("Informe os numeros que desejar. Digite 0 para finalizar");
-		while ((numLer = scanner.nextInt()) != 0) {
-			numeros.add(numLer);
-		}
-		System.out.println("Fim da recuperacao dos numeros\n");
-
-		// usando programacao declativa
-		System.out.println("Numeros informados:");
-		numeros.stream().collect(Collectors.toList()).forEach(System.out::println);
-	}
-
+        // usando programacao declativa
+        System.out.println("Numeros informados:");
+        numeros.stream().toList().forEach(System.out::println);
+    }
 }
